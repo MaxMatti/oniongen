b36df8a6c4b6baf4257bc72ec22691a207416f6d
Max Staff <max.staff@gmx.de>
2016-12-26 21:23:15 +0100

    implemented sha1 and call to openssl's sha1 but mine doesn't work correctly yet.

--- main.cc
+++ main.cc
@@ -4,13 +4,35 @@
 #include <random>
 #include <string>
 #include <cstring>
+#include <cstdint>
+#include <climits>
 #include <functional>
 #include <algorithm>
 
+#include <openssl/sha.h>
+
 #define RAND_INIT 1000
+#define MAXLEN 10
 
 template <typename T> T fastCeil(T denominator, T divisor) {
 	return (denominator + divisor - 1) / divisor;
+}
+
+template <typename T> T swap_endian(T u) {
+	static_assert (CHAR_BIT == 8, "CHAR_BIT != 8");
+
+	union {
+		T u;
+		unsigned char u8[sizeof(T)];
+	} source, dest;
+
+	source.u = u;
+
+	for (size_t k = 0; k < sizeof(T); k++) {
+		dest.u8[k] = source.u8[sizeof(T) - k - 1];
+	}
+
+	return dest.u;
 }
 
 std::string getRandomStr(size_t length) {
@@ -23,105 +45,6 @@ std::string getRandomStr(size_t length) {
 	return result;
 }
 
-// implementation of the s function as described in section 3. of spec
-int32_t sha1_helper_s(int32_t input, unsigned char offset) {
-	return (input << offset) | (input >> (32 - offset));
-}
-
-// implementation of the f function as described in section 5. of spec
-int32_t sha1_helper_f(unsigned char nr, const int32_t& b, const int32_t& c, const int32_t& d) {
-	if (nr < 20) {
-		return (b & c) | ((b ^ 0xFFFFFFFF) & d);
-	} else if (nr < 40) {
-		return b ^ c ^ d;
-	} else if (nr < 60) {
-		return (b & c) | (b & d) | (c & d);
-	} else if (nr < 80) {
-		return b ^ c ^ d;
-	} else {
-		return 0;
-	}
-}
-
-// representation of the K variables as described in section 5. of spec
-int32_t sha1_helper_K(unsigned char nr) {
-	if (nr < 20) {
-		return 0x5A827999;
-	} else if (nr < 40) {
-		return 0x6ED9EBA1;
-	} else if (nr < 60) {
-		return 0x8F1BBCDC;
-	} else if (nr < 80) {
-		return 0xCA62C1D6;
-	} else {
-		return 0;
-	}
-}
-
-// calculates the sha1 sum, allocates 128bit (aka 16 byte) 0-terminated buffer for result (that needs to be freed!)
-// returns nullptr on failure.
-char* sha1(char* input_buffer, const size_t& input_buffer_size) {
-	// copying input_buffer to own storage area with larger size:
-	const size_t input_size = (input_size + 73) & 0xFFFFFFC0;
-	// 73 because 512bit blocks (64bytes) and ending in length (64bit aka 8 bytes) and 1 byte because of padding starting with 0b10000000
-	// 73 = 64 + 8 + 1
-	// then applying floor function
-	unsigned char* input = (unsigned char*) calloc(input_size, sizeof(unsigned char));
-	if (!input) {
-		return input;
-	} // return nullptr if calloc failed.
-	std::memcpy(input, input_buffer, input_buffer_size);
-	
-	// 4. filling up input buffer according to spec
-	*(input + input_buffer_size) = 0x80; // set first bit to 1, others to 0
-	{
-		int64_t tmp = input_buffer_size << 3;
-		memcpy(input + input_size - 8, &tmp, 8);
-	}
-	
-	// 6.1 actual hash algorithm:
-	
-	// initializing result buffer (h0-h4):
-	unsigned int32_t result[] = {0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476, 0xc3d2e1f0};
-	
-	// initializing block buffer, tmp "word" and "words" A-E as described in 6.2
-	unsigned int32_t* current_block = (unsigned int32_t*) calloc(80, sizeof(unsigned int32_t));
-	if (!current_block) {
-		return current_block;
-	} // return nullptr if calloc failed.
-	unsigned int32_t tmp[] = {0, 0, 0, 0, 0, 0}; // tmp and then a-e
-	
-	// processing block by block
-	for (size_t i = 0; i < input_size; i += 64) {
-		
-		// copy current block to buffer
-		memcpy(current_block, input + i, 64);
-		
-		// 6.2 (b) calculate the rest of the current block
-		for (size_t j = 16; j < 80; ++j) {
-			current_block[j] = sha1_helper_s(current_block[j - 3] ^ current_block[j - 8] ^ current_block[j - 14] ^ current_block[j - 16], 1);
-		}
-		
-		// 6.2 (c) fill a-e
-		memcpy(tmp + 1, result, 5 * sizeof(int32_t));
-		
-		// 6.2 (d) wobble around
-		for (unsigned char j = 0; j < 80; ++j) {
-			tmp[0] = sha1_helper_s(tmp[1], 5) + sha1_helper_f(j, tmp[2], tmp[3], tmp[4]) + tmp[5] + current_block[j] + sha1_helper_K(j);
-			// TODO: next line in spec is "E = D;  D = C;  C = S^30(B);  B = A; A = TEMP;" at page 7 under 6.2 (d)
-		}
-	}
-	
-	return input;
-}
-
-std::string sha1(const std::string& input) {
-	char* ptr = sha1(input.c_str(), input.size());
-	std::string result(ptr, 20);
-	free(ptr);
-	return result;
-}
-
 std::string base16(std::string input) {
 	std::string result(fastCeil((size_t) input.size() * 8, (size_t) 4), 0); // 8 bytes in a "normal" string, 4 bytes in a base16-encoded (hex) string
 	char characters[] = "0123456789abcdef";
@@ -130,6 +53,16 @@ std::string base16(std::string input) {
 		result[i * 2 + 1] = characters[input[i] & 15];
 	}
 	return result;
+}
+
+std::string base16(const char* input) {
+	std::string input_str(input, 20);
+	return base16(input_str);
+}
+
+std::string base16(const unsigned char* input) {
+	std::string input_str(reinterpret_cast<const char*>(input), 20);
+	return base16(input_str);
 }
 
 std::string base32(std::string input, char padding) {
@@ -149,7 +82,6 @@ std::string base32(std::string input, char padding) {
 		result[output_pos + 6] = characters[((input[i + 3] << 3) & 24) | ((input[i + 4] >> 5) & 7)]; // 24 base10 = 11000 base2, 7 base10 = 00111 base2
 		result[output_pos + 7] = characters[input[i + 4] & 31]; // 31 base10 = 11111 base2
 	}
-	std::cerr << input.size() << " - " << estimated_output_size << " - " << final_output_size << std::endl;
 	result.resize(final_output_size);
 	result.append(8 - result.size() % 8, padding);
 	return result;
@@ -159,11 +91,166 @@ std::string base32(std::string input) {
 	return base32(input, '=');
 }
 
+std::string base32(const char* input) {
+	std::string input_str(input);
+	return base32(input_str, '=');
+}
+
+std::string base32(const unsigned char* input) {
+	std::string input_str(reinterpret_cast<const char*>(input));
+	return base32(input_str, '=');
+}
+
+// implementation of the s function as described in section 3. of spec
+std::uint32_t sha1_helper_s(std::uint32_t input, unsigned char offset) {
+	return (input << offset) | (input >> (32 - offset));
+}
+
+// implementation of the f function as described in section 5. of spec
+std::uint32_t sha1_helper_f(unsigned char nr, const std::uint32_t& b, const std::uint32_t& c, const std::uint32_t& d) {
+	if (nr < 20) {
+		return (b & c) | ((b ^ 0xFFFFFFFF) & d);
+	} else if (nr < 40) {
+		return b ^ c ^ d;
+	} else if (nr < 60) {
+		return (b & c) | (b & d) | (c & d);
+	} else if (nr < 80) {
+		return b ^ c ^ d;
+	} else {
+		return 0;
+	}
+}
+
+// representation of the K variables as described in section 5. of spec
+std::uint32_t sha1_helper_K(unsigned char nr) {
+	if (nr < 20) {
+		return 0x5A827999;
+	} else if (nr < 40) {
+		return 0x6ED9EBA1;
+	} else if (nr < 60) {
+		return 0x8F1BBCDC;
+	} else if (nr < 80) {
+		return 0xCA62C1D6;
+	} else {
+		return 0;
+	}
+}
+
+// calculates the sha1 sum, allocates 160bit (aka 20 byte) buffer for result (that needs to be freed!)
+// returns nullptr on failure.
+unsigned char* sha1(const char* input_buffer, const size_t& input_buffer_size) {
+	// in case the machine uses big endian we need to swap some bytes later:
+	bool convert_endians;
+	{
+		std::uint32_t endian = 0x08040201;
+		if (*reinterpret_cast<unsigned char*>(&endian) == 1) {
+			convert_endians = 1;
+		} else if (*reinterpret_cast<unsigned char*>(&endian) == 8) {
+			convert_endians = 0;
+		} else {
+			return nullptr;
+		}
+	}
+	// copying input_buffer to own storage area with larger size:
+	const size_t input_size = (input_buffer_size + 73) & 0xFFFFFFC0;
+	// 73 because 512bit blocks (64bytes) and ending in length (64bit aka 8 bytes) and 1 byte because of padding starting with 0b10000000
+	// 73 = 64 + 8 + 1
+	// then applying floor function
+	unsigned char* input = (unsigned char*) calloc(input_size, sizeof(unsigned char));
+	if (!input) {
+		return input;
+	} // return nullptr if calloc failed.
+	std::memcpy(input, input_buffer, input_buffer_size);
+	
+	// 4. filling up input buffer according to spec
+	*(input + input_buffer_size) = 0x80; // set first bit to 1, others to 0
+	{
+		std::uint64_t tmp;
+		if (convert_endians) { // convert endianness in case of big endian
+			tmp = swap_endian<std::uint64_t>(input_buffer_size << 3);
+		} else {
+			tmp = input_buffer_size << 3;
+		}
+		memcpy(input + input_size - 8, &tmp, 8);
+		// These are to check wether the input string was corrupted:
+		std::string a(reinterpret_cast<char*>(input), input_size);
+		std::cerr << base16(a) << std::endl;
+	}
+	
+	// 6.1 actual hash algorithm:
+	
+	// initializing result buffer (h0-h4):
+	std::uint32_t* result = (std::uint32_t*) malloc(5 * sizeof(uint32_t));
+	result[0] = 0x67452301;
+	result[1] = 0xefcdab89;
+	result[2] = 0x98badcfe;
+	result[3] = 0x10325476;
+	result[4] = 0xc3d2e1f0;
+	
+	// initializing block buffer, tmp "word" and "words" A-E as described in 6.2
+	std::uint32_t* current_block = (std::uint32_t*) calloc(80, sizeof(std::uint32_t));
+	if (!current_block) {
+		return reinterpret_cast<unsigned char*>(current_block);
+	} // return nullptr if calloc failed.
+	std::uint32_t tmp[] = {0, 0, 0, 0, 0, 0}; // tmp and then a-e
+	
+	// processing block by block
+	for (size_t i = 0; i < input_size; i += 64) {
+		
+		// copy current block to buffer
+		memcpy(current_block, input + i, 64);
+
+		// convert endianness in case of big endian
+		for (size_t j = 0; j < 64 && convert_endians; ++j) {
+			current_block[j] = swap_endian<std::uint32_t>(current_block[j]);
+		}
+		
+		// 6.2 (b) calculate the rest of the current block
+		for (size_t j = 16; j < 80; ++j) {
+			current_block[j] = sha1_helper_s(current_block[j - 3] ^ current_block[j - 8] ^ current_block[j - 14] ^ current_block[j - 16], 1);
+		}
+		
+		// 6.2 (c) fill a-e
+		memcpy(tmp + 1, result, 5 * sizeof(int32_t));
+		
+		// 6.2 (d) wobble around
+		for (unsigned char j = 0; j < 80; ++j) {
+			tmp[0] = sha1_helper_s(tmp[1], 5) + sha1_helper_f(j, tmp[2], tmp[3], tmp[4]) + tmp[5] + current_block[j] + sha1_helper_K(j);
+			tmp[5] = tmp[4];
+			tmp[4] = tmp[3];
+			tmp[3] = sha1_helper_s(30, tmp[2]);
+			tmp[2] = tmp[1];
+			tmp[1] = tmp[0];
+		}
+
+		// 6.2 (e) wobble around a little bit more
+		result[0] += tmp[1];
+		result[1] += tmp[2];
+		result[2] += tmp[3];
+		result[3] += tmp[4];
+		result[4] += tmp[5];
+	}
+	// convert endianness in case of big endian
+	for (size_t j = 0; j < 5 && convert_endians; ++j) {
+		result[j] = swap_endian<std::uint32_t>(result[j]);
+	}
+	return reinterpret_cast<unsigned char*>(result);
+}
+
+std::string sha1(const std::string& input) {
+	unsigned char* ptr = sha1(input.c_str(), input.size());
+	std::string result(reinterpret_cast<char*>(ptr), 20);
+	free(ptr);
+	return result;
+}
+
 int main() {
 	srand(RAND_INIT);
 	std::string tmp;
-	for (size_t i = 0; i < 20; ++i) {
+	unsigned char hash[20];
+	for (size_t i = 0; i < MAXLEN; ++i) {
 		tmp = getRandomStr(i);
-		std::cout << "\"" << base32(tmp) << "\": \"" << tmp << "\"" << std::endl;
+		SHA1(reinterpret_cast<const unsigned char*>(tmp.c_str()), tmp.size(), hash);
+		std::cout << "\"" << base16(hash) << "\" - \"" << base16(sha1(tmp)) << "\": \"" << tmp << "\"" << std::endl;
 	}
 }
